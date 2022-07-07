970789233eefec061f4963415e45d1b2bcf3232d
Juha Keponen <juha.keponen@cgi.com>
2022-07-07 12:14:59 +0300

    obsolete files

--- local_dev_env/scripts/5_delete_docker_machine_mac.sh
+++
@@ -1,5 +0,0 @@
-# Stop docker-machine (start only when needed)
-docker-machine stop tiesu
-
-# Delete docker-machine
-docker-machine rm tiesu
--- local_dev_env/scripts/6_ssh_to_tomcat_win10.ps1
+++
@@ -1,5 +0,0 @@
-# Get machines env-variables
-& docker-machine env tiesu | Invoke-Expression
-
-# Ssh to tomcat
-docker exec -t -i tomcat /bin/bash
